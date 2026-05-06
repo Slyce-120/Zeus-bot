6f45c7fa33f8e394102b8c93a593ff9a7d796a84
Slyce20 <doxxing478@gmail.com>
2026-05-06 18:02:29 +0200

    Update menu-strumenti.js

--- plugins/menu-strumenti.js
+++ plugins/menu-strumenti.js
@@ -7,7 +7,7 @@ const handler = async (message, { conn, usedPrefix = '.' }) => {
   const totalUsers = Object.keys(global.db?.data?.users || {}).length
 
   const menuBody = `
-『 𝚫𝐗𝐈𝐎𝐍 • 𝐒𝐓𝐑𝐔𝐌𝐄𝐍𝐓𝐈 』
+『 ℤ𝕖𝕦𝕤 • 𝐒𝐓𝐑𝐔𝐌𝐄𝐍𝐓𝐈 』
 ╼━━━━━━━━━━━━━━╾
   ◈ *ᴜsᴇʀ:* @${userId.split('@')[0]}
   ◈ *ᴜᴘᴛɪᴍᴇ:* ${uptimeStr}
@@ -40,7 +40,7 @@ const handler = async (message, { conn, usedPrefix = '.' }) => {
   await conn.sendMessage(message.chat, {
     text: menuBody,
     mentions: [userId],
-    footer: '> *𝛥𝐗𝐈𝐎𝐍 𝚩𝚯𝐓*',
+    footer: '> *ℤ𝕖𝕦𝕤 𝔹𝕠𝕥*',
     buttons: [
       {
         buttonId: `${usedPrefix}menu`,
